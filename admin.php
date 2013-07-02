80500d84ae44b6a3fa4b5993258e23e096aac800
Wouter Vandenneucker <woutervddn@gmail.com>
2013-07-02 17:01:16 +0200

    add the admin page

---
+++ admin.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Gallery</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+		<link rel="stylesheet" href="css/normalize.min.css">
+		<link rel="stylesheet" href="css/main.css"> 
+		<link rel="stylesheet" href="css/gallery.css"> 
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+		
+	</head>
+    <body>
+	
+	<?php
+	require "dbconfig.php";
+	$connection = mysqli_connect($host,$username,$password,$dbname);
+	if (mysqli_connect_errno($connection))
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}	  
+	$result = mysqli_query($connection,"SELECT * FROM clients");
+	
+
+	
+
+	?>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+		<!-- the gallery-->
+		<div class = "maindiv">
+			
+			<!-- demo images -->
+			<div id="Gallery" class="gallery"> 
+
+			<?php
+			
+				while($row = mysqli_fetch_array($result))
+				{
+				
+				if ($row["username"] != null)
+				{
+					echo "<div onclick= loadPhoto('". $row["id"] . "')>" . $row["username"] . "</div>";
+				}
+				$temp = "img_".$row["id"];
+				echo '<div class="imgitem" id = '. $temp .' style=display:none><a href="images/full/' . $row['image'] . '"><img width="200" height="200" src="images/thumb/' . $row['image'] . ' " alt="Hair" /></a></div><br />' ;
+				}
+				mysqli_close($connection);
+			?>
+			</div> 
+		
+		</div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <!-- Include photoswipe files + jQuery -->
+		<link href="css/photoswipe.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="script/klass.min.js"></script> 
+		<script type="text/javascript" src="script/code.photoswipe.jquery-3.0.5.min.js"></script> 
+		
+        <script src="js/main.js"></script>
+        
+        <script type="text/javascript">		
+			$(document).ready(function(){ var myPhotoSwipe = $("#Gallery a").photoSwipe({ enableMouseWheel: false , enableKeyboard: false }); }); 
+			function loadPhoto(id){
+				$("#img_" + id).show();
+			}
+			</script>
+			
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
